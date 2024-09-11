c0339c39e92cfe65a67bd76897e474421e9141fa
ala <ala.alssadah@gmail.com>
2024-09-11 20:14:35 +0300

    feat: add installed  to package

---
+++ packages/aalssadah/crud-generator/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'aalssadah/crud-generator',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'e3f72e7249fb8045b421ea79a1ba600e15680856',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'aalssadah/crud-generator' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'e3f72e7249fb8045b421ea79a1ba600e15680856',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
